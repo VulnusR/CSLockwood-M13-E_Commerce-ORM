e4de65b85950017575843bf2259a45205271d251
Caleb Lockwood <caleblockwood@iMac.local>
2023-03-10 11:12:42 -0700

    began definging tables in ./db/schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -4,3 +4,10 @@ DROP DATABASE IF EXISTS ecommerce_db;
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
 
+-- Categories table
+CREATE TABLE categories (
+  id INT NOT NULL AUTO_INCREMENT,
+  category_name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
+);
+
