2c6a7e184a1059974553b04a6ab1516e279b9811
Caleb Lockwood <caleblockwood@iMac.local>
2023-03-13 20:16:32 -0700

    tag-routes.js updated

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -2,15 +2,41 @@ const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
+http://localhost:3001/api/tags
 
-router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+router.get('/', async (req, res) => {
+  try {
+
+    const tags = await Tag.findAll({
+      include: Product
+    });
+
+    res.status(200).json(tags);
+  } 
+  
+  catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+router.get('/:id', async (req, res) => {
+  try {
+
+    const tag = await Tag.findByPk(req.params.id, {
+      include: Product
+    });
+
+    if (!tag) {
+      res.status(404).json({ message: 'No tag found with this id' });
+      return;
+    }
+
+    res.status(200).json(tag);
+  } 
+  
+  catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
