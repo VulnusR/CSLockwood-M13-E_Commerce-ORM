465c3488f86194073f85d96f9998c6353ec0333f
Caleb Lockwood <caleblockwood@iMac.local>
2023-03-13 20:12:14 -0700

    product routes.js delete

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -107,7 +107,22 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+  try {
+    const product = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!product) {
+      res.status(404).json({ message: 'No product found with this id' });
+      return;
+    }
+
+    res.status(200).json({ message: 'Product deleted successfully' });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
